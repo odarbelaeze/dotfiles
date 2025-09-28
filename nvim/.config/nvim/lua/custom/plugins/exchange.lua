2e8482d8805266f19765be5ed858ea8a1d01970d
Oscar Arbelaez <oscar@arbelaez.dev>
2025-09-28 12:01:57 +0100

    remove old configuration files

--- nvim/.config/nvim/lua/custom/plugins/exchange.lua
+++
@@ -1,3 +0,0 @@
-return {
-  'tommcdo/vim-exchange',
-}
--- nvim/.config/nvim/lua/custom/plugins/github.lua
+++
@@ -1,9 +0,0 @@
-return {
-  'Almo7aya/openingh.nvim',
-  event = 'VeryLazy',
-  config = function()
-    vim.api.nvim_set_keymap('n', '<Leader>gr', ':OpenInGHRepo <CR>', { silent = true, noremap = true })
-    vim.api.nvim_set_keymap('n', '<Leader>gf', ':OpenInGHFile <CR>', { silent = true, noremap = true })
-    vim.api.nvim_set_keymap('v', '<Leader>gf', ':OpenInGHFileLines <CR>', { silent = true, noremap = true })
-  end,
-}
